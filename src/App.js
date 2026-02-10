37be97615f60b594ec41dfe776e9a87570fbfb80
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-10 15:20:52 +0530

    feat : create product link using Link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home></Home>}></Route>
-    <Route path="/products" element={<Product></Product>}></Route>
-  </Route>,
-);
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home></Home>}></Route>
+//     <Route path="/products" element={<Product></Product>}></Route>
+//   </Route>,
+// );
 
-const router = createBrowserRouter(routeDefinations);
+// const router = createBrowserRouter(routeDefinations);
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home></Home> },
-//   { path: "/products", element: <Product></Product> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home></Home> },
+  { path: "/products", element: <Product></Product> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1>My Home Page.</h1>
+      <p><Link to='/products'>go to products page</Link></p>
     </div>
   );
 };
