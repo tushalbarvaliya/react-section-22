c8300b95abd63569fe8e62274b77991c54e2627c
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-10 15:08:30 +0530

    feat : add second page product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 
 const router = createBrowserRouter([
-  {path:'/',element:<Home></Home>}
+  {path:'/',element:<Home></Home>},
+  {path:'/products',element:<Product></Product>},
 ])
 
 function App() {
