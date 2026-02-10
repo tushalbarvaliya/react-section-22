323d98b3d90f6857fff2001f446a8a94ee5c6d5d
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-10 16:30:59 +0530

    fix : make dynamic link for products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+const PRODUCTS = [
+  { id: "p1", title: "product- 1 " },
+  { id: "p2", title: "product- 2 " },
+  { id: "p3", title: "product- 3 " },
+];
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <main>
+      <div>Product</div>
+      <ul>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+        <li></li>
+      </ul>
+    </main>
+  );
 }
 
-export default Product
+export default Product;
